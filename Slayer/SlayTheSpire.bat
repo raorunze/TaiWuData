f23290380f9a055bf8efa5bebc61891ac8a5ee18
raorunze <37732121+raorunze@users.noreply.github.com>
2018-10-31 16:08:16 +0800

    Salayer移除jar,增加语句

--- Slayer/SlayTheSpire.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-jre\bin\java.exe -cp simple.jar;desktop-1.0.jar -Xmx1G -Dsun.java2d.dpiaware=true -javaagent:byteman.jar=script:common.btm,script:Ironclad.btm,script:TheSilent.btm,script:Defect.btm com.megacrit.cardcrawl.desktop.DesktopLauncher
-
